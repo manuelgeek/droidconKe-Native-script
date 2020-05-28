1331c1efa35e80c3f5a403942fe5f815e0700c80
manuelgeek <emashmagak@appslab.co.ke>
2020-05-28 23:34:25 +0300

    added session preview

--- app/components/home_page/partials/SessionsPreview.vue
+++ app/components/home_page/partials/SessionsPreview.vue
@@ -1,12 +1,68 @@
 <template>
-    <StackLayout orientation="horizontal" class="w-full mt-3">
-        <StackLayout class="w-2/3">
-            <Label text="Sessions" fontSize="24" class="w-full black font-bold font-weight-bold text-left"/>
+    <StackLayout>
+        <StackLayout orientation="horizontal" class="w-full mt-3">
+            <StackLayout class="w-2/3">
+                <Label text="Sessions" fontSize="24" class="w-full black font-bold font-weight-bold text-left"/>
+            </StackLayout>
+            <StackLayout orientation="horizontal" class="w-1/3 text-right">
+                <Label text="View All" fontSize="16" verticalAlignment="center" class="w-1/2 purple font-bold mr-2"/>
+                <Label text="+12" fontSize="14" class="w-1/2 purple bg-purple-light-c text-center rounded-full px-3 py-1"/>
+            </StackLayout>
         </StackLayout>
-        <StackLayout orientation="horizontal" class="w-1/3 text-right">
-            <Label text="View All" fontSize="16" class="w-1/2 purple font-bold mr-2"/>
-            <Label text="+12" fontSize="14" class="w-1/2 purple bg-purple-light-c text-center rounded-full px-3 py-2"/>
-        </StackLayout>
+        <ScrollView orientation="horizontal" class="w-full mt-3 mb-2">
+            <StackLayout orientation="horizontal" height="270">
+                <StackLayout orientation="vertical" class="w-2/3 rounded-lg bg-white-c mr-3">
+                    <Image src="~/assets/images/Greg Keynote.png"  class="w-full" height="160" verticalAlignment="center" />
+                    <DockLayout class="px-3 mt-2">
+                        <StackLayout orientation="horizontal" dock="left" class="w-1/2 text-left">
+                            <Image src.decode="font://&#xf017;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="10:30 AM" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                        <StackLayout orientation="horizontal" dock="right" class="w-1/2 text-right">
+                            <Image src.decode="font://&#xf277;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="Room 1" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                    </DockLayout>
+                    <StackLayout horizontalAlignment="left" class="px-3 mt-3">
+                        <Label text="Community on a global scale." fontSize="20" textWrap="true" width="280" class="font-bold gray text-left"/>
+                    </StackLayout>
+                </StackLayout>
+
+                <StackLayout orientation="vertical" class="w-2/3 rounded-lg bg-white-c mr-3">
+                    <Image src="~/assets/images/Dawit Abraham.png"  class="w-full" height="160" verticalAlignment="center" />
+                    <DockLayout class="px-3 mt-2">
+                        <StackLayout orientation="horizontal" dock="left" class="w-1/2 text-left">
+                            <Image src.decode="font://&#xf017;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="10:30 AM" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                        <StackLayout orientation="horizontal" dock="right" class="w-1/2 text-right">
+                            <Image src.decode="font://&#xf277;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="Room 1" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                    </DockLayout>
+                    <StackLayout horizontalAlignment="left" class="px-3 mt-3">
+                        <Label text="Launching your app for the African market." textWrap="true" width="280" fontSize="20" class="font-bold gray text-left"/>
+                    </StackLayout>
+                </StackLayout>
+
+                <StackLayout orientation="vertical" class="w-2/3 rounded-lg bg-white-c mr-3">
+                    <Image src="~/assets/images/Greg Keynote.png"  class="w-full" height="160" verticalAlignment="center" />
+                    <DockLayout class="px-3 mt-2">
+                        <StackLayout orientation="horizontal" dock="left" class="w-1/2 text-left">
+                            <Image src.decode="font://&#xf017;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="10:30 AM" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                        <StackLayout orientation="horizontal" dock="right" class="w-1/2">
+                            <Image src.decode="font://&#xf277;" class="fas t-36 green mr-2" width="20"></Image>
+                            <Label text="Room 1" fontSize="16" class="gray uppercase"/>
+                        </StackLayout>
+                    </DockLayout>
+                    <StackLayout horizontalAlignment="left" class="px-3 mt-3">
+                        <Label text="Community on a global scale." fontSize="20" textWrap="true" width="280" class="font-bold gray text-left"/>
+                    </StackLayout>
+                </StackLayout>
+            </StackLayout>
+        </ScrollView>
     </StackLayout>
 </template>
 
